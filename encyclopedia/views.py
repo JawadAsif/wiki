cbbd3640c1b71533fda97066ac06573c57275c74
Md. Jawad Noor Asif <jawad.asif.bd@gmail.com>
2020-07-13 22:03:15 +0600

    show search results for already added entries

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
 from . import markdown2
 from . import util
 
@@ -18,3 +18,20 @@ def entry(request, title):
         })
     else:
         return render(request, "encyclopedia/entry_not_found.html")
+
+
+def search(request):
+    if request.method == "POST":
+        print("here")
+        searched_text = request.POST["q"]
+        print(searched_text)
+        entries = [entry.lower() for entry in util.list_entries()]
+        print(entries)
+        if searched_text in entries:
+            return HttpResponseRedirect(reverse("entry",kwargs={'title': searched_text}))
+        else:
+            result = [s for s in entries if searched_text in s]
+            print(result)
+            return render(request, "encyclopedia/searched_result.html", {
+                          "results": result
+                          })
